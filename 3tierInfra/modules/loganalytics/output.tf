e7638f4cd969b83158ba79bf9874e58740cf8eb1
raullingaraj <raullingaraj@gmail.com>
2022-12-01 13:51:49 +0530

    Delete 3tierInfra directory

--- 3tierInfra/modules/loganalytics/output.tf
+++
@@ -1,9 +0,0 @@
-output "workspace_id" {
-  value       = azurerm_log_analytics_workspace.main.id
-  description = "Id of the workspace"
-}
-
-output "workspace_name" {
-  value       = azurerm_log_analytics_workspace.main.name
-  description = "Id of the workspace"
-}
