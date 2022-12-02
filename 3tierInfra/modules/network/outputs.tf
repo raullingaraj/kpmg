b9167e0ead95433020d46a4555fcefd2f13504db
raullingaraj <raullingaraj@gmail.com>
2022-12-02 11:27:36 +0530

    Delete 3tierInfra directory

--- 3tierInfra/modules/network/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newl vNet"
-  value       = azurerm_subnet.subnet.*.id
-}
--- 3tierInfra/modules/sqldb/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "sql_private_ip_address" {
-  value = azurerm_network_interface.sqlnic.private_ip_address
-}
